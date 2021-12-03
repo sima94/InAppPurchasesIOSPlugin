55ef59429ce1b9e12a1e183252a709cd546e23c9
Stefan Simic <stefan.simic.twint@interventure.info>
2021-12-03 20:17:21 +0100

    Initial commit

---
+++ android/src/main/java/com/mmsd/nfb/iap/InAppPurchases.java
@@ -0,0 +1,11 @@
+package com.mmsd.nfb.iap;
+
+import android.util.Log;
+
+public class InAppPurchases {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
